11eaee898e0c1a0d3b2ab9d346d9bcbf46952313
Erik <erik@arenhill.com>
2021-03-23 11:24:00 +0100

    feat: add timestamp to logger

--- src/util/logger.util.js
+++ src/util/logger.util.js
@@ -1,6 +1,7 @@
 const os = require('os');
 const fs = require('fs');
 const { LOGS, STORAGE_PATH } = require('../constants');
+const time = require('./time.util')
 
 let logStream = false;
 
@@ -9,21 +10,21 @@ module.exports.log = (message, config = {}) => {
     if (!logStream)
       logStream = fs.createWriteStream(`${STORAGE_PATH}/messages.log`, { flags: 'a' });
 
-    const logMessage =
-      typeof message === 'string'
+    const logMessage = `${time.current()}: ` +
+      (typeof message === 'string'
         ? message.replace(/\n/g, os.EOL)
-        : JSON.stringify(message, null, '\t');
+        : JSON.stringify(message, null, '\t'));
     logStream.write(logMessage + os.EOL);
 
     if (config.verbose) {
       if (LOGS === 'verbose') {
-        console.log(message);
+        console.log(logMessage);
         // if (config.dashes) this.dashes(message);
       }
       return;
     }
 
-    console.log(message);
+    console.log(logMessage);
     // if (config.dashes) this.dashes(message);
   } catch (error) {
     console.log('logger error');
